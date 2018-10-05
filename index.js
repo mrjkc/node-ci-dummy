ed1fb76c2c261d386695dd662784821be4afb4de
mac-key <juliuskvedaras@yahoo.ie>
2018-10-05 14:43:33 +0100

    testing eror

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {getForecast, mapPrettifyResults} = require('./lib/weather')
 
-module.exports = (req, res) => {
+mo\dule.exports = (req, res) => {
   getForecast('Stuttgart,DE')
     .then(result => result.map(mapPrettifyResults))
     .then(weather => res.end(JSON.stringify(weather, null, 2)))
